58be28887bf40983368b89b507fdfdb4bdf84e5f
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-14 23:17:15 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,101 +1,100 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-// console.log(process.env.OPENAI_API_KEY);
+console.log(process.env.OPENAI_API_KEY);
 
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.status(200).send("Hello Code Ai server is started")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Code Ai server is started");
+});
 
 // converts the following code from one lang to other
 
-app.post('/convert', async (req, res) => {
-    try {
-        const prompt = req.body.prompt
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-            stop: ["\"\"\""],
-        })
+app.post("/convert", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      stop: ['"""'],
+    });
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error })
-    }
-})
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
 // explain the code functions
 
-app.post('/explain', async (req, res) => {
-    try {
-        const prompt = req.body.prompt
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-            stop: ["\"\"\""],
-        })
+app.post("/explain", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      stop: ['"""'],
+    });
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error })
-    }
-})
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
 // find the error in the code
-app.post('/error', async (req, res) => {
-    try {
-        const prompt = req.body.prompt
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-            stop: ["\"\"\""],
-        })
+app.post("/error", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      stop: ['"""'],
+    });
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error })
-    }
-})
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
 app.listen(5000, () => {
-    console.log("Server is started")
-})
+  console.log("Server is started");
+});
